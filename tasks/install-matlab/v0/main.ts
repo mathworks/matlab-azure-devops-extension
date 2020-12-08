fee3fef22e21136dc6f599b7775211b22d806ac9
Mark Cafaro <34887852+mcafaro@users.noreply.github.com>
2020-12-08 09:21:40 -0500

    Updated doc with new selectBy* options (#35)
    * Added doc for new run tests selectBy parameters
    
    * Made release option truly optional
    
    * Corrected JUnit casing in doc

--- tasks/install-matlab/v0/main.ts
+++ tasks/install-matlab/v0/main.ts
@@ -8,28 +8,35 @@ import {platform} from "./utils";
 async function run() {
     try {
         taskLib.setResourcePath(path.join( __dirname, "task.json"));
-        const release = taskLib.getInput("release", true);
-        await install(release as string);
+        const release = taskLib.getInput("release");
+        await install(release);
     } catch (err) {
         taskLib.setResult(taskLib.TaskResult.Failed, err.message);
     }
 }
 
-async function install(release: string) {
+async function install(release?: string) {
     const serverType = taskLib.getVariable("System.ServerType");
     if (!serverType || serverType.toLowerCase() !== "hosted") {
         throw new Error(taskLib.loc("InstallNotSupportedOnSelfHosted"));
     }
 
     // install core system dependencies
-    let exitCode = await curlsh("https://ssd.mathworks.com/supportfiles/ci/matlab-deps/v0/install.sh", release);
+    const depArgs: string[] = [];
+    if (release !== undefined) {
+        depArgs.push(release);
+    }
+    let exitCode = await curlsh("https://ssd.mathworks.com/supportfiles/ci/matlab-deps/v0/install.sh", depArgs);
     if (exitCode !== 0) {
         throw new Error(taskLib.loc("FailedToExecuteInstallScript", exitCode));
     }
 
     // install ephemeral version of MATLAB
-    exitCode = await curlsh("https://ssd.mathworks.com/supportfiles/ci/ephemeral-matlab/v0/ci-install.sh",
-        ["--release", release]);
+    const installArgs: string[] = [];
+    if (release !== undefined) {
+        installArgs.push("--release", release);
+    }
+    exitCode = await curlsh("https://ssd.mathworks.com/supportfiles/ci/ephemeral-matlab/v0/ci-install.sh", installArgs);
     if (exitCode !== 0) {
         throw new Error(taskLib.loc("FailedToExecuteInstallScript", exitCode));
     }
