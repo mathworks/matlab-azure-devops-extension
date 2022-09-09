c1d1955c09402d25bccb83e36f99e9772a43e311
mw-kapilg <kapilg@mathworks.com>
2022-09-09 11:01:40 +0530

    Updated tests

--- tasks/run-matlab-tests/v0/test/common.ts
+++ tasks/run-matlab-tests/v0/test/common.ts
@@ -25,7 +25,11 @@ export function runCmdArg(
             "'SelectByTag','" + selectByTag + "'," +
             "'CoberturaModelCoverage','" + modelCoverageCobertura + "'," +
             "'SimulinkTestResults','" + testResultsSimulinkTest + "'," +
-            "'PDFTestReport','" + testResultsPDF + "');" +
+            "'PDFTestReport','" + testResultsPDF + "'," +
+            "'UseParallel','" + useParallel + "'," +
+            "'Strict','" + strict + "'," +
+            "'LoggingLevel','" + loggingLevel + "'," +
+            "'OutputDetail','" + outputDetail + "');" +
         `disp('Running MATLAB script with contents:');` +
         `disp(testScript.Contents);` +
         `fprintf('__________\\n\\n');` +
