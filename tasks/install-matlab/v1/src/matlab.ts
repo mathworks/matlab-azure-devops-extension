e7ebb3bd38886755c42b789c1c3998e6c727b4d8
David Buzinski <davidb@mathworks.com>
2023-01-10 16:17:51 -0500

    fixing some spacing and minor touchups

--- tasks/install-matlab/v1/src/matlab.ts
+++ tasks/install-matlab/v1/src/matlab.ts
@@ -26,15 +26,11 @@ export async function makeToolcacheDir(release: Release): Promise<[string, boole
 export async function getReleaseInfo(release: string): Promise<Release> {
     // Get release name from input parameter
     let name: string;
-    if ( release.toLowerCase().trim() === "latest") {
-        try {
-            name = await resolveLatest();
-        } catch {
-            return Promise.reject(Error(`Unable to retrieve the MATLAB release information. Contact MathWorks at continuous-integration@mathworks.com if the problem persists.`));
-        }
+    if (release.toLowerCase().trim() === "latest") {
+        name = await resolveLatest();
     } else {
         const nameMatch = release.toLowerCase().match(/r[0-9]{4}[a-b]/);
-        if ( !nameMatch ) {
+        if (!nameMatch) {
             return Promise.reject(Error(`${release} is invalid or unsupported. Specify the value as R2020a or a later release.`));
         }
         name = nameMatch[0];
@@ -43,7 +39,7 @@ export async function getReleaseInfo(release: string): Promise<Release> {
     // create semantic version of format year.semiannual.update from release
     const year = name.slice(1, 5);
     const semiannual = name[5] === "a" ? "1" : "2";
-    const updateMatch = release.toLowerCase().match(/u[0-9]/);
+    const updateMatch = release.toLowerCase().match(/u[0-9]+/);
     let version = `${year}.${semiannual}`;
     let update: string;
     if (updateMatch) {
@@ -65,7 +61,7 @@ async function resolveLatest(): Promise<string> {
     return new Promise((resolve, reject) => {
         https.get("https://ssd.mathworks.com/supportfiles/ci/matlab-release/v0/latest", (resp) => {
             if (resp.statusCode !== 200) {
-                reject(Error(`Unable to retrieve the MATLAB release information. Contact MathWorks at continuous-integration@mathworks.com if the problem persists.`));
+                reject(Error(`Unable to retrieve latest MATLAB release information. Contact MathWorks at continuous-integration@mathworks.com if the problem persists.`));
             }
             resp.on("data", (d) => {
                 resolve(d.toString());
