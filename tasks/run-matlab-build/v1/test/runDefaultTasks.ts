040d3e68b855589a36bf6f996a7d04e7adf21385
David Buzinski <davidb@mathworks.com>
2024-02-14 13:21:21 -0500

    Revert "reremove"
    This reverts commit e066e33def6f1e9ac0153b1b749bf24a9cfe0f53.

---
+++ tasks/run-matlab-build/v1/test/runDefaultTasks.ts
@@ -0,0 +1,30 @@
+// Copyright 2022 The MathWorks, Inc.
+
+import ma = require("azure-pipelines-task-lib/mock-answer");
+import mr = require("azure-pipelines-task-lib/mock-run");
+import path = require("path");
+
+const tp = path.join(__dirname, "..", "main.js");
+const tr = new mr.TaskMockRunner(tp);
+
+const runCmdPath = path.join(path.dirname(__dirname), "bin", "glnxa64", "run-matlab-command");
+
+tr.registerMock("./utils", {
+    platform: () => "linux",
+    architecture: () => "x64",
+});
+
+const a: ma.TaskLibAnswers = {
+    checkPath: {
+        [runCmdPath]: true,
+    },
+    exec: {
+        [runCmdPath + " buildtool"]: {
+            code: 0,
+            stdout: "ran default tasks",
+        },
+    },
+} as ma.TaskLibAnswers;
+tr.setAnswers(a);
+
+tr.run();
---
+++ tasks/run-matlab-tests/v1/test/runTestsWithStartupOptsLinux.ts
@@ -0,0 +1,45 @@
+// Copyright 2023 The MathWorks, Inc.
+
+import ma = require("azure-pipelines-task-lib/mock-answer");
+import mr = require("azure-pipelines-task-lib/mock-run");
+import path = require("path");
+import { runCmdArg } from "./common";
+
+const tp = path.join(__dirname, "..", "main.js");
+const tr = new mr.TaskMockRunner(tp);
+
+tr.setInput("testResultsJUnit", "results.xml");
+tr.setInput("codeCoverageCobertura", "coverage.xml");
+tr.setInput("sourceFolder", "source");
+tr.setInput("selectByFolder", "tests/filteredTest");
+tr.setInput("selectByTag", "FILTERED");
+tr.setInput("modelCoverageCobertura", "modelcoverage.xml");
+tr.setInput("testResultsSimulinkTest", "stmresults.mldatx");
+tr.setInput("testResultsPDF", "results.pdf");
+tr.setInput("strict", "true");
+tr.setInput("useParallel", "true");
+tr.setInput("outputDetail", "Verbose");
+tr.setInput("loggingLevel", "Verbose");
+tr.setInput("startupOptions", "-nojvm -nodisplay");
+
+const runCmdPath = path.join(path.dirname(__dirname), "bin", "glnxa64", "run-matlab-command");
+
+tr.registerMock("./utils", {
+    platform: () => "linux",
+    architecture: () => "x64",
+});
+
+const a: ma.TaskLibAnswers = {
+    checkPath: {
+        [runCmdPath]: true,
+    },
+    exec: {
+        [runCmdPath + " " + runCmdArg("results.xml", "coverage.xml", "source", "tests/filteredTest", "FILTERED", "modelcoverage.xml", "stmresults.mldatx", "results.pdf", true, true, "Verbose", "Verbose") + " -nojvm -nodisplay"]: {
+            code: 0,
+            stdout: "ran tests",
+        },
+    },
+} as ma.TaskLibAnswers;
+tr.setAnswers(a);
+
+tr.run();
