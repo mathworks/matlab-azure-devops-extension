2c262e02f85ab2ff02e5e159102b2fdf2c687e55
David Buzinski <davidb@mathworks.com>
2024-02-14 11:53:01 -0500

    readd run-tests and run-build

---
+++ tasks/run-matlab-build/v1/test/runBuildWindows.ts
@@ -0,0 +1,32 @@
+// Copyright 2022 The MathWorks, Inc.
+
+import ma = require("azure-pipelines-task-lib/mock-answer");
+import mr = require("azure-pipelines-task-lib/mock-run");
+import path = require("path");
+
+const tp = path.join(__dirname, "..", "main.js");
+const tr = new mr.TaskMockRunner(tp);
+
+tr.setInput("tasks", "test");
+
+const runCmdPath = path.join(path.dirname(__dirname), "bin", "win64", "run-matlab-command.exe");
+
+tr.registerMock("./utils", {
+    platform: () => "win32",
+    architecture: () => "x64",
+});
+
+const a: ma.TaskLibAnswers = {
+    checkPath: {
+        [runCmdPath]: true,
+    },
+    exec: {
+        [runCmdPath + " buildtool test"]: {
+            code: 0,
+            stdout: "ran test task",
+        },
+    },
+} as ma.TaskLibAnswers;
+tr.setAnswers(a);
+
+tr.run();
