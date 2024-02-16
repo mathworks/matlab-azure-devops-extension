ddebc279dae8cbef7e3a7ee314b7b45b214bfa2f
David Buzinski <davidb@mathworks.com>
2024-02-16 10:44:11 -0500

    trying downgrade azure-pipelines-task-lib

--- scripts/compile.js
+++ scripts/compile.js
@@ -5,7 +5,7 @@
 const project = require("./project");
 const sh = require("shelljs");
 
-sh.config.fatal = true;
+// sh.config.fatal = true;
 
 for (let task of project.taskList) {
     sh.echo(`> compiling ${task.fullName}`);
