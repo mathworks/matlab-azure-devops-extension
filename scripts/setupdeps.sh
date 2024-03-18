19500fbeb1a913867295b4676fbfffddacc10c82
David Buzinski <103441853+davidbuzinski@users.noreply.github.com>
2024-03-18 10:10:52 -0400

    Update scripts/setupdeps.sh
    Co-authored-by: Mark Cafaro <34887852+mcafaro@users.noreply.github.com>

--- scripts/setupdeps.sh
+++ scripts/setupdeps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RMC_BASE_URL="https://ssd.mathworks.com/supportfiles/ci/run-matlab-command/$1"
+VERSION={$1:-v2}
+RMC_BASE_URL="https://ssd.mathworks.com/supportfiles/ci/run-matlab-command/$VERSION"
 SUPPORTED_OS=('win64' 'maci64' 'glnxa64')
 
 # Create dist directory if it doesn't already exist
