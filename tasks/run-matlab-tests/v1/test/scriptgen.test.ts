e16adf7721cf99cb02157451fcb968152501fd75
Vahila <70003902+Vahila@users.noreply.github.com>
2025-10-10 20:54:59 +0530

    Update failing test

--- tasks/run-matlab-tests/v1/test/scriptgen.test.ts
+++ tasks/run-matlab-tests/v1/test/scriptgen.test.ts
@@ -24,6 +24,8 @@ export default function suite() {
 
             const actual = scriptgen.generateCommand(options);
 
+            console.log(actual);
+            
             assert(actual.includes("genscript('Test'"));
             assert(actual.includes("'JUnitTestResults',''"));
             assert(actual.includes("'CoberturaCodeCoverage',''"));
@@ -66,6 +68,8 @@ export default function suite() {
 
             const actual = scriptgen.generateCommand(options);
 
+            console.log(actual);
+            
             assert(actual.includes("genscript('Test'"));
             assert(actual.includes("'JUnitTestResults','test-results/results.xml'"));
             assert(actual.includes("'CoberturaCodeCoverage','code-coverage/coverage.xml'"));
